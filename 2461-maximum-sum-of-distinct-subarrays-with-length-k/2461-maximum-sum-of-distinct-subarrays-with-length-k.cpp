6bc1ce64e4f3b49cb30c4d3b092f0a885b628882
Tanmay Paliwal <tanmayprofessional576@gmail.com>
2026-08-06 14:33:04 +0530

    Time: 100 ms (63.13%), Space: 95.4 MB (47.67%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -3,41 +3,30 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
 
         unordered_map<int, int> freq;
-        set<int> st;
-
-        long long sum = 0;
-
-        // First window
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        long long totalsum = 0;
+        long long ans = 0;
+        for(int i =0 ; i<k ; i++)
+        {
+            totalsum+=nums[i];
             freq[nums[i]]++;
-            st.insert(nums[i]);
         }
 
-        long long maxsum = 0;
+        if(freq.size() == k) ans = totalsum;
 
-        if (st.size() == k)
-            maxsum = sum;
-
-        // Sliding window
-        for (int j = k; j < nums.size(); j++) {
-
-            sum += nums[j];
+        for(int j = k ; j<nums.size() ; j++)
+        {
+            freq[nums[j-k]]--;
+            if(freq[nums[j-k]] == 0)
+                freq.erase(nums[j-k]);
+            totalsum+=nums[j];
+            totalsum-=nums[j-k];
             freq[nums[j]]++;
-            st.insert(nums[j]);
 
-            sum -= nums[j - k];
-            freq[nums[j - k]]--;
-
-            if (freq[nums[j - k]] == 0) {
-                freq.erase(nums[j - k]);
-                st.erase(nums[j - k]);
-            }
-
-            if (st.size() == k)
-                maxsum = max(maxsum, sum);
+            if(freq.size() == k) ans =  max(totalsum , ans);
         }
 
-        return maxsum;
+        return ans;
+
+        
     }
 };
