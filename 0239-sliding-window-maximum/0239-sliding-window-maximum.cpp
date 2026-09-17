5f3330114c259119436e084d7816aa93b4082255
Tanmay Paliwal <tanmayprofessional576@gmail.com>
2026-09-17 21:19:05 +0530

    Time: 16 ms (84.84%), Space: 139.1 MB (73.78%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,25 +4,23 @@ public:
         deque<int> dq;
         vector<int> result;
 
-        for(int right = 0 ; right<nums.size();right++)
+        for(int right = 0 ; right < nums.size() ; right++)
         {
-            while(!dq.empty() && nums[dq.back()] < nums[right])
+            while(!dq.empty() && nums[right] > dq.back())
             {
                 dq.pop_back();
             }
+            dq.push_back(nums[right]);
 
-            dq.push_back(right);
-
-            if(dq.front() <= right - k)
+            if(right>=k && dq.front()==nums[right-k])
             {
                 dq.pop_front();
             }
 
-
-            if(right >= k - 1)
-                result.push_back(nums[dq.front()]);
-
-
+            if(right>=k-1)
+            {
+                result.push_back(dq.front());
+            }
 
         }
 
