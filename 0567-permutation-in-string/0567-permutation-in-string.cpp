fc75765fa89d10326a1b24becdf253c84aabed26
Tanmay Paliwal <tanmayprofessional576@gmail.com>
2026-09-22 19:54:31 +0530

    Time: 0 ms (100%), Space: 10.7 MB (27%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length())
+        unordered_map<int,int> freq;
+        unordered_map<int,int> freq_of_s2;
+
+        for(int i=0 ; i<s1.length();i++)
+        {
+            freq[s1[i]]++;
+        }
+
+        int right = 0;
+        int left = 0;
+        int count = 0;
+
+        while(right < s2.length())  
+        {
+            
+                freq_of_s2[s2[right]]++;
+        
+                count++;
+
+            while(count > s1.length())
+            {
+                freq_of_s2[s2[left]]--;
+                if(freq_of_s2[s2[left]] == 0)
+                    freq_of_s2.erase(s2[left]);
+
+                left++;
+                count--;              
+            }       
+
+             if(freq == freq_of_s2) 
+            {
+                return true;
+            }
+            
+            right++;
+        }
+
+         
             return false;
 
-        vector<int> a(26, 0);
-        vector<int> b(26, 0);
-
-        for (int i = 0; i < s1.length(); i++) {
-            a[s1[i] - 'a']++;
-            b[s2[i] - 'a']++;
-        }
-
-        if (a == b)
-            return true;
-
-        int left = 0;
-
-        for (int right = s1.length(); right < s2.length(); right++) {
-            b[s2[right] - 'a']++;
-            b[s2[left] - 'a']--;
-
-            left++;
-
-            if (a == b)
-                return true;
-        }
-
-        return false;
     }
 };
