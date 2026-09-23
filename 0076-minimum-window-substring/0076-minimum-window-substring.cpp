1d9a8f1a1cf0d958afc38d7e321de04c39155d2b
Tanmay Paliwal <tanmayprofessional576@gmail.com>
2026-09-24 01:29:16 +0530

    Time: 19 ms (23.21%), Space: 11.6 MB (27.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,46 +1,67 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int need[128] = {0};
 
-        for (char c : t) {
-            need[c]++;
+        unordered_map<int,int> freq_oft;
+        unordered_map<int,int> freq_ofs;
+
+        for(int i = 0; i < t.length(); i++)
+        {
+            freq_oft[t[i]]++;
         }
 
         int left = 0;
-        int count = t.size();
+        int right = 0;
+
+        int formed = 0;
+        int required = freq_oft.size();
+
         int start = 0;
-        int minLen = INT_MAX;
+        int minLength = INT_MAX;
 
-        for (int right = 0; right < s.size(); right++) {
+        while(right < s.length())
+        {
+            freq_ofs[s[right]]++;
 
-            if (need[s[right]] > 0) {
-                count--;
+            if(freq_oft.find(s[right]) != freq_oft.end() &&
+               freq_ofs[s[right]] == freq_oft[s[right]])
+            {
+                formed++;
             }
 
-            need[s[right]]--;
-
-            while (count == 0) {
-
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
+            while(formed == required)
+            {
+                // Store only the position and length
+                if(right - left + 1 < minLength)
+                {
                     start = left;
+                    minLength = right - left + 1;
                 }
 
-                need[s[left]]++;
+                if(freq_oft.find(s[left]) != freq_oft.end() &&
+                   freq_ofs[s[left]] == freq_oft[s[left]])
+                {
+                    formed--;
+                }
 
-                if (need[s[left]] > 0) {
-                    count++;
+                freq_ofs[s[left]]--;
+
+                if(freq_ofs[s[left]] == 0)
+                {
+                    freq_ofs.erase(s[left]);
                 }
 
                 left++;
             }
+
+            right++;
         }
 
-        if (minLen == INT_MAX) {
+        if(minLength == INT_MAX)
+        {
             return "";
         }
 
-        return s.substr(start, minLen);
+        return s.substr(start, minLength);
     }
 };
